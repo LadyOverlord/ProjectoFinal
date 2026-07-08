466d27e2dc95f6d93362c377f1b29682675ac545
Linn-002 <santoslinia@gmail.com>
2026-07-08 16:22:20 +0100

    Corrigir nome do arquivo termos

--- WEB/Scripts/firebase.js
+++ WEB/Scripts/firebase.js
@@ -1,28 +1,4 @@
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-firestore.js";
